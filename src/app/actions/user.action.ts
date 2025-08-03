49e4c4f598b3c5c28086a326d26442608c2042e6
animesh <phb21013@tezu.ac.in>
2025-08-03 15:25:30 +0530

    trying to fix prisma edge issue -> apparentlyserver action won't work with prisma in good way git add . server action sucks

--- src/app/actions/user.action.ts
+++ src/app/actions/user.action.ts
@@ -1,30 +1,30 @@
-'use server'
+"use server";
+export const runtime = "nodejs";
 
-import { signupObject, SignupTypes } from "@/lib/config/user.config"
-import { signup } from "@/lib/data/user"
-import { handleError } from "@/lib/error"
+import { signupObject, SignupTypes } from "@/lib/config/user.config";
+import { signup } from "@/lib/data/user";
+import { handleError } from "@/lib/error";
 
-export const createUser = async (data : SignupTypes) => {
+export const createUser = async (data: SignupTypes) => {
     try {
-        const parseddata = signupObject.safeParse(data)
+        const parseddata = signupObject.safeParse(data);
 
-        if(!parseddata.success){
-            throw parseddata.error
+        if (!parseddata.success) {
+            throw parseddata.error;
         }
 
-        await signup(parseddata.data)
+        await signup(parseddata.data);
 
         return {
-            success : true,
-            message : "User Created successfully"
-        }
+            success: true,
+            message: "User Created successfully",
+        };
     } catch (error) {
-        
-        const err = handleError(error)
+        const err = handleError(error);
         return {
-            success : false,
-            message : err.message,
-            status : err.status
-        }
+            success: false,
+            message: err.message,
+            status: err.status,
+        };
     }
-}
+};
